8e954ad81c386a1fc7ede99c220b1396d4b69f1c
betulerkoc <bet.er@hotmail.com>
2022-12-19 22:52:42 +0300

    update content

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,3 @@
-import Cotton from "../../Cotton.png";
 import Cloth from "../../Cloth.png";
 import { Button } from "@material-tailwind/react";
 import { InstagramEmbed } from "react-social-media-embed";
@@ -7,38 +6,65 @@ import Form from "../Form";
 export default function Home() {
   return (
     <>
-      <div className="bg-black w-screen h-96 mt-10 flex flex-col items-center justify-evenly">
-        <img className="w-20 h-20 xl:w-48 xl:h-48" src={Cotton} alt="cotton" />
-        <p className="text-white text-2xl md:text-3xl xl:text-4xl text-center px-2">
-          - It takes 2,700 liters of water <span className="italic">That’s enough drinking water for one person for 900 days{" "}</span>
-        </p>
-        <p className="text-white text-2xl md:text-3xl xl:text-4xl text-center px-2">
-          - The approximate carbon footprint for a T-shirt made of polyester is 35
-          kg-CO2e/kg
-        </p>
+      <div className="bg-green-200 py-5 w-screen h-fit mt-10 flex flex-col items-center justify-evenly lg:px-20">
+      <img className="w-28 h-28" src="https://img.icons8.com/dotty/80/null/jacket.png" alt="jacket"/>
+        <div className="border w-40 h-64 bg-white flex flex-col">
+          <div className="border border-dashed py-4">
+            <p className="text-center font-bold">ENVIRONMENTAL SAVINGS</p>
+            <p className="text-center text-xs">OF RECYCLED CONTENT IN THE RAW MATERIALS</p>
+            <p className="text-center text-xs font-bold">Calculation per 1kg</p>
+          </div>
 
+          <div className="flex flex-row py-2 justify-around items-center">
+            <img
+              className="w-7 h-7"
+              src="https://img.icons8.com/ios/50/null/wet.png"
+              alt="water"
+            />
+            <p>396 LITERS</p>
+          </div>
+          <div className="flex flex-row py-1 justify-around">
+            <img
+              className="w-7 h-7"
+              src="https://img.icons8.com/external-line-rakhmat-setiawan/64/null/external-carbondioxide-ecology-and-energy-line-line-rakhmat-setiawan.png"
+              alt="co2"
+            />
+            <p>42 CO2</p>
+          </div>
+          <div className="flex flex-row py-1 justify-around">
+            <img
+              className="w-7 h-7"
+              src="https://img.icons8.com/ios/50/null/electricity.png"
+              alt="energy"
+            />
+            <p>83.5 kWh</p>
+          </div>
+        </div>
       </div>
-      <div className="w-screen h-fit my-5 flex flex-col lg:flex-row items-center justify-around">
+      <div className="w-screen h-fit my-5 flex flex-col lg:flex-row lg:px-20 items-center justify-around">
         <img
           className="w-20 h-20 xl:w-36 xl:h-36 mt-10"
           src={Cloth}
           alt="cloth"
         />
-        <p className="text-4xl my-2 font-bold text-center">
+        <p className="text-4xl my-2 font-bold text-center lg:ml-5">
           Fast Fashion is out of Fashion
         </p>
-        <div className="text-xl md:text-2xl xl:text-2xl text-center">
+        <div className="text-xl lg:text-2xl lg:px-5 text-center">
           <p className="p-2">
-            Global textiles production almost doubled between 2000 and 2015
+            - Global textiles production almost doubled between 2000 and 2015
           </p>
           <p className="p-2">
-            About 5.8 million tonnes of textiles are discarded every year in the
-            EU, approximately 11kg per person
+            - About 5.8 million tonnes of textiles are discarded every year in
+            the EU, approximately 11kg per person
           </p>
-          <p className="p-2">
-            Every second somewhere in the world a truckload of textiles is
-            landfilled or incinerated
-          </p>
+          <Button
+            variant="outlined"
+            className="text-black border-black my-3"
+            onClick={() => window.gtag("event", "form_submit")}
+          >
+            See More
+          </Button>
         </div>
       </div>
       <p className="text-4xl font-bold mt-10 text-center"> Useful Videos</p>
@@ -97,34 +123,44 @@ export default function Home() {
         ></iframe>
       </div>
 
-      <p className="text-4xl font-bold my-3"> Combine Tips</p>
-      <div className="flex flex-wrap  justify-around py-10">
-        <InstagramEmbed
-          url="https://www.instagram.com/p/ClTtvTCKriQ/"
-          captioned
-          className="w-auto h-auto mr-10 mt-3"
-        />
-        <InstagramEmbed
-          url="https://www.instagram.com/p/Ce_XDysKo9C/"
-          captioned
-          className="w-auto h-auto mr-10 mt-3"
-        />
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-4xl font-bold mt-10 text-center"> Combine Tips</p>
+        <div className="flex flex-wrap justify-around py-5">
+          <InstagramEmbed
+            url="https://www.instagram.com/p/ClTtvTCKriQ/"
+            captioned
+            className="w-auto h-auto mt-3"
+          />
+          <InstagramEmbed
+            url="https://www.instagram.com/p/Ce_XDysKo9C/"
+            captioned
+            className="w-auto h-auto mt-3"
+          />
+        </div>
+        <Button
+          variant="outlined"
+          className="text-black border-black w-32 mb-5"
+          onClick={() => window.gtag("event", "form_submit")}
+        >
+          See More
+        </Button>
       </div>
-      <Button variant="outlined" className="text-black border-black">
-        See More
-      </Button>
 
       <div className="flex flex-col items-center my-4">
         <p className="text-4xl font-bold my-4 text-center"> DIY Suggestions</p>
         <iframe
-          className="w-full h-80 my-3"
+          className="w-full h-80 my-3 lg:w-1/2"
           src="https://www.youtube.com/embed/fj7vja5DxZ4"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
-        <Button variant="outlined" className="text-black border-black">
+        <Button
+          variant="outlined"
+          className="text-black border-black mb-5"
+          onClick={() => window.gtag("event", "form_submit")}
+        >
           See More
         </Button>
       </div>
